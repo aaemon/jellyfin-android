a6ff63a7e5aadb15505a953f6271e605538cf344
Ammer Ashraf Emon <30506675+aaemon@users.noreply.github.com>
2023-08-21 14:00:59 +0000

    updated

--- app/src/main/java/org/jellyfin/mobile/JellyfinApplication.kt
+++ app/src/main/java/org/jellyfin/mobile/JellyfinApplication.kt
@@ -29,6 +29,18 @@ class JellyfinApplication : Application() {
             if (isWebViewSupported()) {
                 WebView.setWebContentsDebuggingEnabled(true)
             }
+
+            // Read the URL, username, and password from the shared preferences
+            val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+            val url = sharedPreferences.getString("url", "https://jellyfin.dotscale.tech")
+            val username = sharedPreferences.getString("username", "carnival")
+            val password = sharedPreferences.getString("password", "121456")
+
+            // If the URL, username, and password are not empty, then login to the Jellyfin server
+            if (url.isNotEmpty() && username.isNotEmpty() && password.isNotEmpty()) {
+                val loginManager = LoginManager(this)
+                loginManager.login(url, username, password)
+            }
         }
 
         startKoin {
